d2b2ef090cd29e81b9ea5c12a52c2e5dce93a934
Max Carlson <max@maxcarlson.com>
2014-09-12 02:59:06 +0200

    Merge branch 'master' of https://github.com/teem2/dreem
    Conflicts:
    docs/api/data-9192c50da81d6cd05b9cfc95f3e58853.js
    docs/api/index.html
    docs/api/output/dr.ace.js
    docs/api/output/dr.audioplayer.js
    docs/api/output/dr.bitmap.js
    docs/api/output/dr.boundslayout.js
    docs/api/output/dr.buttonbase.js
    docs/api/output/dr.checkbutton.js
    docs/api/output/dr.dataset.js
    docs/api/output/dr.dragstate.js
    docs/api/output/dr.dreem_iframe.js
    docs/api/output/dr.gyro.js
    docs/api/output/dr.inputtext.js
    docs/api/output/dr.labelbutton.js
    docs/api/output/dr.labeltoggle.js
    docs/api/output/dr.layout.js
    docs/api/output/dr.node.js
    docs/api/output/dr.replicator.js
    docs/api/output/dr.shim.js
    docs/api/output/dr.simplelayout.js
    docs/api/output/dr.slider.js
    docs/api/output/dr.state.js
    docs/api/output/dr.text.js
    docs/api/output/dr.touch.js
    docs/api/output/dr.view.js
    docs/api/source/layout.html

--- classdocs.js
+++ classdocs.js
@@ -240,14 +240,42 @@
      * @extends dr.view
      * iframe component for embedding dreem code or html in a dreem application.
      * The size of the iframe matches the width/height of the view when the
-     * component is created.
-     * The file '/iframe_stub.html' contains the html to load into the iframe,
-     * including the required script elements to run dreem code.
+     * component is created. The iframe component can show a web page by
+     * using the src attribute, or to show dynamic content using the
+     * contents attribute.
+     *
+     * This example shows how to display a web page in an iframe. The 
+     * contents of the iframe are not editable:
+     *
+     *     @example
+     *     <dreem_iframe src="http://en.wikipedia.org/wiki/San_Francisco" width="400" height="150"></dreem_iframe>
+     *
+     * To make the web page clickable, and to add scrolling:
+     *
+     *     @example
+     *     <dreem_iframe src="http://en.wikipedia.org/wiki/San_Francisco" width="400" height="150" scrolling="true" clickable="true"></dreem_iframe>
+     *
+     * The content of the iframe can also be dynamically generated, including
+     * adding Dreem code:
+     *
+     *     @example
+     *     <dreem_iframe width="300" height="150" contents="Hello"></dreem_iframe>
+     *
      */
 /**
+        * @cfg {String} [src="/iframe_stub.html"]
+        * url to load inside the iframe. By default, a file is loaded that has
+        * an empty body but includes the libraries needed to support Dreem code.
+        */
+/**
+        * @cfg {Boolean} [scrolling="false"]
+        * Controls scrollbar display in the iframe.
+        */
+/**
         * @cfg {String} [contents=""]
-        * string to write into the iframe body. This is dreem code, or html
-        * that is written inside the iframe's body tag.
+        * string to write into the iframe body. This is dreem/html code
+        * that is written inside the iframe's body tag. If you want to display
+        * static web pages, specify the src attribute, but do not use contents.
         */
 /**
      * @class dr.gyro
