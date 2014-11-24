2290278474ddd8c1f11b1884deb8d1e2bd5a110c
Lorien Henry-Wilkins <lorien.henrywilkins@gmail.com>
2014-11-24 12:08:07 -0800

    [#82011786] moved manual test to specs

---
+++ spec/features/handlers_spec.rb
@@ -0,0 +1,20 @@
+require 'spec_helper'
+
+describe 'handlers', :type => :feature, :js => true do
+
+  before :each do
+    visit '/spec/features/handlers.html'
+    wait_for_javascript_event 'dreemInited'
+  end
+  
+  describe 'by reference' do
+    
+    it 'a handler can be attached by reference to a node that is a sibling of its parent' do
+      find("#v1").click
+      expect(page.evaluate_script("window.v1clicked")) == true
+      
+      find("#v2").click
+      expect(page.evaluate_script("window.v2clicked")) == true
+    end
+  end
+end
