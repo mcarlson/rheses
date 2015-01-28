614beddcaa8478f1f847aa0d80399e9007e036fc
gnovos <freemason@gmail.com>
2015-01-28 11:48:39 -0800

    fix tests to match new UI

--- spec/features/checkbutton_spec.rb
+++ spec/features/checkbutton_spec.rb
@@ -10,7 +10,7 @@ describe 'checkbutton', :type => :feature, :js => true do
   it 'box color and selected value change when clicked' do
     expect(page.evaluate_script('pinkbtn.selected')).to be false
     find("#pinkbtn").click
-    expect(find("#pinkbtn").find('view').native.css_value('background-color')).to eq 'rgba(255, 192, 203, 1)'
+    expect(find("#pinkbtn").find('art').native.css_value('background-color')).to eq 'rgba(255, 192, 203, 1)'
     expect(page.evaluate_script('pinkbtn.selected')).to be true
   end
 end
