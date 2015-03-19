2f2740af6fcb1b3047d46a07b8a6edec2ec6fc9f
Lorien Henry-Wilkins <lorien.henrywilkins@gmail.com>
2015-03-19 15:23:53 -0700

    [#79231422] api doc example for the teem component

--- classdocs.js
+++ classdocs.js
@@ -1597,6 +1597,24 @@
      * @class dr.teem {Util, Events}
      * @extends dr.node
      * Connects to the shared event bus. When data is sent with a given type, a corresponding event is sent. For example, send('blah', {}) sends data with the 'blah' type, other shims will receive the object via an 'onblah' event.
+     *
+     * Here is an example of sending data via dr.teem. View the example in mutiple devices and see the value update in both.
+     *
+     *     @example
+     *     <teem id="bus">
+     *       <handler event="onrandnum" args="data">
+     *          output.setAttribute('text', data.txt);
+     *       </handler>
+     *     </teem>
+     *     
+     *     <spacedlayout axis="y"></spacedlayout>
+     *
+     *     <labelbutton text="send message">
+     *        <handler event="onclick">
+     *          bus.send('randnum', {"txt":'another random number: ' + Math.floor(Math.random() * 100000)});
+     *        </handler>
+     *     </labelbutton>
+     *     <text id="output"></text>
      */
 /**
         * @attribute {Number} [pingtime=5000]
